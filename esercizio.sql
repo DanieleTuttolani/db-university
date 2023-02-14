2ba827abd2c20a9c6fd44d3a3939e70d794282f9
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-14 15:50:35 +0100

    finito

--- esercizio.sql
+++ esercizio.sql
@@ -39,12 +39,22 @@ WHERE `phone` is NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*) AS 'anno', enrolment_date
+FROM `students`
+GROUP BY `enrolment_date`;
 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT COUNT(*) AS 'professori', `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT AVG(`vote`) AS 'media voto' , `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(`department_id`) AS 'n corsi' , 'tipo di indirizzo'
+FROM`degrees`
+GROUP BY `department_id`;
