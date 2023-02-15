4ef854036dd16913f59a2995bf53a6b2f426ee62
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-15 13:55:34 +0100

    2/3

--- esercizio2.sql
+++ esercizio2.sql
@@ -2,10 +2,22 @@
 SELECT `students`. `name`, `students`. `surname`, `degrees`. `name`
 FROM`students`
 JOIN `degrees`
-ON `degrees`.id = `degree_id`
+ON `degrees`.id = `students`. `degree_id`
 WHERE `degrees`. `name` LIKE '%Economia%';
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `departments`. `name`, `degrees`. `name` AS 'degrees name'
+FROM `departments`
+JOIN`degrees`
+ON `departments`. `id` = `degrees`.`department_id`
+WHERE `departments`. `id` = 7;
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `teachers`.`id`,  `teachers`.`name` , `teachers`.`surname`,`courses`.`name` AS 'nome corso'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id`= `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id`= `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44;
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
